abe78c150e0114f4172c39f685d464e98cdcc93d
scoobie <rolando.a@sapo.pt>
2020-12-03 16:27:57 +0000

    SellerFormViewModel and Department select component

---
+++ SalesWebMvc/Models/ViewModels/SellerFormVieModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormVieModel
+    {
+        public Seller Seller{ get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
